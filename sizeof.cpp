62b0e1caa52c23bd279e18b326877d3bb5868847
valeena019 <74050930+valeena019@users.noreply.github.com>
2021-08-20 15:04:48 +0530

    Add files via upload

---
+++ sizeof.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"size of char is"<<sizeof(char)<<"byte";
+	cout<<"size of int is"<<sizeof(int)<<"byte";
+	cout<<"size of  float is"<<sizeof(float)<<"byte";
+	cout<<"size of double is"<<sizeof(double)<<"byte";
+	return 0;
+}
